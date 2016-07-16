9c0b3f1c109c69de9ae593242b844369bb05f2e0
Alexander Buhr <alex86011119@googlemail.com>
2016-07-16 11:58:31 +0200

    header map (WIP)

--- src/main/java/net/brainified/vertx_websockets/Verticle.java
+++ src/main/java/net/brainified/vertx_websockets/Verticle.java
@@ -3,18 +3,19 @@ package net.brainified.vertx_websockets;
 import java.text.DateFormat;
 import java.time.Instant;
 import java.util.Date;
+import java.util.Objects;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.ext.web.handler.sockjs.BridgeEventType;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
-import io.vertx.ext.web.handler.sockjs.SockJSHandlerOptions;
 
 final class Verticle extends AbstractVerticle {
 
@@ -25,12 +26,10 @@ final class Verticle extends AbstractVerticle {
   public void start(final Future<Void> fut) {
     final EventBus eb = vertx.eventBus();
 
-    // Register to listen for messages coming IN to the server
     eb.consumer("chat.to.server").handler(message -> {
-      // Create a timestamp string
       String timestamp = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(Date.from(Instant.now()));
-      // Send the message back out to all clients with the timestamp prepended.
-      eb.publish("chat.to.client", timestamp + ": " + message.body());
+      System.out.println(message.headers().size());
+      eb.publish("chat.to.client", timestamp + ", " + message.headers().get("remoteAddress") + ": " + message.body());
     });
 
     final BridgeOptions opts = new BridgeOptions()
@@ -41,14 +40,30 @@ final class Verticle extends AbstractVerticle {
       System.out.println(event.type());
       System.out.println(event.socket().remoteAddress());
       if (BridgeEventType.SOCKET_CREATED.equals(event.type())) {
-        eb.publish("chat.to.client", event.type().toString() +" " + event.socket().remoteAddress().toString());
+        //JsonObject message = new JsonObject();
+        //message.put("address", "chat.to.server");
+        //message.put("body", "test");
+        //message.put("type", "send");
+        //event.setRawMessage(message);
+        //eb.publish("chat.to.client", event.type().toString() +" " + event.socket().remoteAddress().toString());
+      }
+      if (BridgeEventType.REGISTER.equals(event.type())) {
+        //eb.publish("chat.to.client", event.type().toString() +" " + event.socket().remoteAddress().toString());
       }
       if (BridgeEventType.SOCKET_CLOSED.equals(event.type())) {
-        eb.publish("chat.to.client", event.type().toString() +" " + event.socket().remoteAddress().toString());
+        //eb.publish("chat.to.client", event.type().toString() +" " + event.socket().remoteAddress().toString());
       }
-
+      if (BridgeEventType.SEND.equals(event.type())) {
+        JsonObject rawMessage = event.getRawMessage();
+        JsonObject headers = rawMessage.getJsonObject("headers");
+        if (Objects.nonNull(headers)) {
+          headers.put("remoteAddress", event.socket().remoteAddress().toString());
+        }
+        System.out.println(Json.encode(rawMessage));
+      }
       event.complete(true);
     });
+    
 
     final Router router = Router.router(vertx);
     router.route("/eventbus/*").handler(sockJSHandler);
@@ -62,8 +77,6 @@ final class Verticle extends AbstractVerticle {
         fut.fail(result.cause());
       }
     });
-    
-
 
   }
 
