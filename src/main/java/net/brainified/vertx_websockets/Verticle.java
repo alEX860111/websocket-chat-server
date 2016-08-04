f40d7a119f6463c6ed302f39f32e56bbcd52bffd
Alexander Buhr <alex86011119@googlemail.com>
2016-08-04 13:30:26 +0200

    Refactored

--- src/main/java/net/brainified/vertx_websockets/Verticle.java
+++ src/main/java/net/brainified/vertx_websockets/Verticle.java
@@ -20,20 +20,11 @@ final class Verticle extends AbstractVerticle {
 
   @Override
   public void start(final Future<Void> fut) {
-    final EventBus eventBus = vertx.eventBus();
-
-    eventBus.consumer("chat.to.server").handler(message -> {
-      String timestamp = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM)
-          .format(Date.from(Instant.now()));
-      eventBus.publish("chat.to.client", timestamp + ", " + message.headers().get("remoteAddress") + ": " + message.body());
-    });
+    configureEventBus();
 
     final SockJSHandler sockJSHandler = createSockJSHandler();
 
-    final Router router = Router.router(vertx);
-    router.route("/eventbus/*").handler(sockJSHandler);
-    router.route("/app/*").handler(StaticHandler.create("../../workspace_js/websocket-chat-client/app"));
-    router.route("/assets/*").handler(StaticHandler.create("../../workspace_js/websocket-chat-client/node_modules"));
+    final Router router = createRouter(sockJSHandler);
 
     vertx.createHttpServer().requestHandler(router::accept).listen(config().getInteger("http.port", 8080), result -> {
       if (result.succeeded()) {
@@ -45,13 +36,22 @@ final class Verticle extends AbstractVerticle {
 
   }
 
+  private void configureEventBus() {
+    final EventBus eventBus = vertx.eventBus();
+
+    eventBus.consumer("chat.to.server").handler(message -> {
+      String timestamp = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM)
+          .format(Date.from(Instant.now()));
+      eventBus.publish("chat.to.client", timestamp + ", " + message.headers().get("remoteAddress") + ": " + message.body());
+    });
+  }
+
   private SockJSHandler createSockJSHandler() {
     final BridgeOptions options = new BridgeOptions()
         .addInboundPermitted(new PermittedOptions().setAddress("chat.to.server"))
         .addOutboundPermitted(new PermittedOptions().setAddress("chat.to.client"));
 
     final SockJSHandler sockJSHandler = SockJSHandler.create(vertx).bridge(options, event -> {
-
       if (BridgeEventType.SEND.equals(event.type())) {
         final JsonObject rawMessage = event.getRawMessage();
         final JsonObject headers = rawMessage.getJsonObject("headers");
@@ -61,7 +61,16 @@ final class Verticle extends AbstractVerticle {
       }
       event.complete(true);
     });
+
     return sockJSHandler;
+  }
+
+  private Router createRouter(final SockJSHandler sockJSHandler) {
+    final Router router = Router.router(vertx);
+    router.route("/eventbus/*").handler(sockJSHandler);
+    router.route("/app/*").handler(StaticHandler.create("../../workspace_js/websocket-chat-client/app"));
+    router.route("/assets/*").handler(StaticHandler.create("../../workspace_js/websocket-chat-client/node_modules"));
+    return router;
   }
 
 }
