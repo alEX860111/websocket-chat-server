6e5fc5f504d9356db68b702ae4e6deec8e9098c7
Alexander Buhr <alex86011119@googlemail.com>
2016-08-10 12:09:42 +0200

    Formatting

--- src/main/resources/app/chat.js
+++ src/main/resources/app/chat.js
@@ -1,18 +1,18 @@
-$(function () {
+$(function() {
   var form = $('#message-form');
   var messageInput = $('#message').focus();
   var chat = $('#chat');
 
   var eb = new EventBus('http://localhost:8080/eventbus');
 
-  eb.onopen = function () {
+  eb.onopen = function() {
 
     // set a handler to receive a message
-    eb.registerHandler('chat.to.client', function (error, message) {
+    eb.registerHandler('chat.to.client', function(error, message) {
       console.log('received a message: ' + JSON.stringify(message));
       console.log(message.body);
       var element = $('<div></div>');
-      //element.addClass('text-left');
+      // element.addClass('text-left');
       element.addClass('text-success');
       element.text(message.body);
       chat.append(element);
